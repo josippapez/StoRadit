682cd2628faf8d8eb10c0fb39e2bc1aaff298a96
Josip <papezjosip@gmail.com>
2021-12-17 11:18:54 +0100

    added types to fix failing workflow

--- src/store/actions/gamesActions.ts
+++
@@ -1,8 +0,0 @@
-import { AppDispatch } from '../store';
-
-/* export const setSearchByName = (state: boolean) => {
-  return (disaptch: AppDispatch): void => {
-    disaptch(gamesSlice.actions.setSearchByName(state));
-  };
-};
- */
